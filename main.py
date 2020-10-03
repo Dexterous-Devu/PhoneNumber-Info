9e995c5ef61699abb6782879282be9e79a63dbab
Dexterous-Devu <62784420+Dexterous-Devu@users.noreply.github.com>
2020-10-03 13:45:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import phonenumbers as ph
+
+__author__ : '@rockdevu'
+
+import phonenumbers as ph     # pip install phonenumbers
 from phonenumbers import carrier
 from phonenumbers import geocoder
 
@@ -7,5 +10,6 @@ ch_number = ph.parse(number, None)
 result_city = geocoder.description_for_number(ch_number, 'de')
 ro_number = ph.parse(number, 'RO')
 result_tele = carrier.name_for_number(ro_number, 'en')
+
 print('Country : ', result_city)
 print('Carrier : ', result_tele)
