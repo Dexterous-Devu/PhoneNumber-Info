19333ce6597c52862fa42ba6e09b3578bd35bfd8
2012devanshu <deepakjain24june@gmail.com>
2020-10-03 13:42:34 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import phonenumbers as ph
+from phonenumbers import carrier
+from phonenumbers import geocoder
+
+number = input("Enter Phone Number : ")
+ch_number = ph.parse(number, None)
+result_city = geocoder.description_for_number(ch_number, 'de')
+ro_number = ph.parse(number, 'RO')
+result_tele = carrier.name_for_number(ro_number, 'en')
+print('Country : ', result_city)
+print('Carrier : ', result_tele)
